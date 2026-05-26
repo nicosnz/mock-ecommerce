1f8a8a064445bc166081776f6cc4420a3cf564a8
Emanuel Oly <olysancheznicolas@gmail.com>
2026-05-25 23:36:58 -0400

    test(frontend) más lineas probadas en addProduct

--- test/product/addProduct.test.ts
+++ test/product/addProduct.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest';
-import { addProduct } from '../../src/products/ProductsMock';
+import { addProduct } from '../../src/products/addProduct';
+import { products } from '../../src/products/ProductsMock';
 
 describe('addProduct', () => {
 
@@ -18,7 +19,8 @@ describe('addProduct', () => {
       stock,
       cat,
       desc,
-      editingId
+      editingId,
+      products
     );
 
     expect(result).toEqual({
@@ -30,7 +32,7 @@ describe('addProduct', () => {
 
   
 
-  it('No debe agregar cuando el precio o stock son menor o igual a 0', () => {
+  it('No debe agregar cuando el precio o stock son igual a 0', () => {
 
     const nombre = 'Teclado';
     const precio = 0;
@@ -45,12 +47,38 @@ describe('addProduct', () => {
       stock,
       cat,
       desc,
-      editingId
+      editingId,
+      products
     );
 
     expect(result).toEqual({
       ok: false,
       message: '⚠️ Por favor dar numeros validos para precio o stock.'
+    });
+
+  });
+  it('No debe agregar cuando el precio o stock son menor a 0', () => {
+
+    const nombre = 'Teclado';
+    const precio = -12;
+    const stock = -23;
+    const cat = 'Tecnologia';
+    const desc = 'Teclado mecanico';
+    const editingId = null;
+
+    const result = addProduct(
+      nombre,
+      precio,
+      stock,
+      cat,
+      desc,
+      editingId,
+      products
+    );
+
+    expect(result).toEqual({
+      ok: false,
+      message: '⚠️ Por favor no introducir numeros negativos.'
     });
 
   });
