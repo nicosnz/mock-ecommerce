e6257f526e48ef136a49c87fd1be46ebdf8007d2
Emanuel Oly <olysancheznicolas@gmail.com>
2026-06-19 01:58:51 -0400

    refactor(cart): renombrar variables del reduce a nombres descriptivos

--- src/cart/updateCartCount.ts
+++ src/cart/updateCartCount.ts
@@ -1,6 +1,6 @@
 import type { Cart } from "./Cart";
 
 export function updateCartCount(cart:Cart[]) {
-  const total = cart.reduce((s, i) => s + i.qty, 0);
+  const total = cart.reduce((acumulado, item) => acumulado + item.qty, 0);
   return total;
 }
